2ed6ef4b34d109778be5016106163d7d011542b4
CaptianHaddock <108212144+captianhaddok@users.noreply.github.com>
2022-08-09 05:53:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,9 +19,9 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94711421243']
-global.premium = ['94711421243']
-global.ownernomer = '94711421243'
+global.owner = ['94760043776']
+global.premium = ['94760043776']
+global.ownernomer = '94760043776'
 global.ownername = 'Dark Alpha'
 global.botname = 'BIXBY-MD USER BOT'
 global.footer = ' Qᴜᴇᴇɴ ʙɪxʙʏ ᴘᴜʙʟɪᴄ ᴇᴅɪᴛɪᴏɴ.'
